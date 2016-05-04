0bbbc2008edb8ac5b2034e06c54c419193d94254
rjhallsted <rjhallsted@gmail.com>
2016-05-03 18:37:33 -0600

    Successfully drawing paddles.

--- initialization.js
+++ initialization.js
@@ -1,6 +1,41 @@
 document.addEventListener( 'DOMContentLoaded', function() {
-	var canvas = document.querySelector('#pongCanvas');
-	var context = canvas.getContext('2d');
+	var fillColor = '#333333';
 
-	
+	var canvas = document.querySelector('#pongCanvas');
+	var cxt = canvas.getContext('2d');
+	cxt.fillStyle = fillColor;
+	cxt.width = canvas.width;
+	cxt.height = canvas.height;
+
+	drawPaddle( cxt, 'left');
+	drawPaddle( cxt, 'right');
 });
+
+
+function drawPaddle( cxt, side ) {
+	var width = 6;
+	var height = 80;
+
+	var topPosition = getPaddleTopPosition( cxt, height );
+
+	var leftPosition = getPaddleLeftPosition( cxt, side, width );
+
+	cxt.fillRect( leftPosition, topPosition, width, height );
+}
+
+function getPaddleLeftPosition( cxt, side, paddleWidth ) {
+	var sideMargin = 30;
+
+	if( side == 'left' ) {
+		return sideMargin;
+	}
+	else
+		return cxt.width - paddleWidth - sideMargin;
+}
+
+function getPaddleTopPosition( cxt, paddleHeight ) {
+	var halfCanvas = cxt.height / 2;
+	var halfPaddle = paddleHeight / 2;
+
+	return halfCanvas - halfPaddle;
+}
